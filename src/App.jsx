4d4df2bdd77e709bae846a44a701b4b509cca9c9
Muhammadqodir <goldenparen06@gmail.com>
2025-04-14 21:30:08 +0500

    dws

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      Assalomu alaykum!
-      WebSayt tez orada ishga tushadi!
+    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 flex items-center justify-center text-white">
+      <div className="text-center">
+         <h1>Websayt tez orada ishga tushadi
+         </h1>
+         <img src="https://t3.ftcdn.net/jpg/02/36/99/22/360_F_236992283_sNOxCVQeFLd5pdqaKGh8DRGMZy7P4XKm.jpg" alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
