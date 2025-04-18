773abf6d781b9c41a5d03c3f067bf3aa0842a64a
Muhammadqodir <goldenparen06@gmail.com>
2025-04-19 01:23:03 +0500

    f

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 flex items-center justify-center text-white">
       <div className="text-center">
-         <h1> Foydasiz , hech narsa yo'q </h1>
-=         <img src="https://t3.ftcdn.net/jpg/02/36/99/22/360_F_236992283_sNOxCVQeFLd5pdqaKGh8DRGMZy7P4XKm.jpg" alt="" />
+         <h1> Foydasiz , hech narsa yo'q hozircha </h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpselB7dJ1ZW4b1YOGCnAmU7Njph2HLwnUHwMeRn_IWbHACG0uUSAxdhfs8mSlSKZqTBs&usqp=CAU" alt="" />
       </div>
     </div>
   );
