9aac9a50efe1012bd11064c3a7235e275f1f3037
Muhammadqodir <goldenparen06@gmail.com>
2025-04-19 01:26:14 +0500

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 flex items-center justify-center text-white">
       <div className="text-center">
-         <h1> Foydasiz , hech narsa yo'q hozircha </h1>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpselB7dJ1ZW4b1YOGCnAmU7Njph2HLwnUHwMeRn_IWbHACG0uUSAxdhfs8mSlSKZqTBs&usqp=CAU" alt="" />
+         <h1 className='mt-5'> Foydasiz , hech narsa yo'q hozircha </h1>
+    <img style={{width: 400}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpselB7dJ1ZW4b1YOGCnAmU7Njph2HLwnUHwMeRn_IWbHACG0uUSAxdhfs8mSlSKZqTBs&usqp=CAU" alt="" />
       </div>
     </div>
   );
