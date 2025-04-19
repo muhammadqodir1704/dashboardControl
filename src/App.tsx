8bcf1d6fc59dfb92452a36635be0ace6c87b2fbd
Muhammadqodir <goldenparen06@gmail.com>
2025-04-19 23:20:32 +0500

    docs: React vite changed to TS v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,3 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 flex items-center justify-center text-white">
-      <div className="text-center">
-         <h1 className='mt-5'> Foydasiz , hech narsa yo'q hozircha </h1>
-    <img style={{width: 400}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpselB7dJ1ZW4b1YOGCnAmU7Njph2HLwnUHwMeRn_IWbHACG0uUSAxdhfs8mSlSKZqTBs&usqp=CAU" alt="" />
-      </div>
-    </div>
-  );
-}
-
-export default App;
+const App: React.FC = () => {
+  return <h1>Hello</h1>;
+};
